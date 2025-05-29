17cf65128d65c7c313396aeed894c40c2c2bc5a9
Ayush Patel <iamayushpatelap@gmail.com>
2025-05-29 10:03:31 +0530

    Day-3

---
+++ Day-3/BooksApi.Services/Services/BookService.cs
@@ -0,0 +1,56 @@
+﻿using BooksApi.Models;
+
+namespace BooksApi.Services
+{
+    // For CRUD on books
+    public class BookService
+    {
+        private List<Book> _books;
+
+        public BookService()
+        {
+            _books = new List<Book>();
+        }
+
+        // To Add Book
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+
+        // To Get All Books
+        public List<Book> GetAll()
+        {
+            return _books;
+        }
+
+        // To Get Single Book
+        public Book? GetBookById(int id)
+        {
+            return _books.Find(x => x.Id == id);
+        }
+
+        // To Update Book
+        public bool UpdateBook(int id, Book updatedBook)
+        {
+            var index = _books.FindIndex(b => b.Id == id);
+            if (index == -1)
+                return false;
+
+            _books[index] = updatedBook;
+            return true;
+        }
+
+        // To Delete Book
+        public bool DeleteBook(int id)
+        {
+            var book = _books.Find(b => b.Id == id);
+            if (book == null)
+                return false;
+
+            _books.Remove(book);
+            return true;
+        }
+
+    }
+}
---
+++ Day-3/BooksApi/Controllers/BookController.cs
@@ -0,0 +1,69 @@
+﻿using BooksApi.Models;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : Controller
+    {
+        private readonly BookService bookService;
+
+        public BookController(BookService book)
+        {
+            this.bookService = book;
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public ActionResult AddBook(Book book)
+        {
+            this.bookService.AddBook(book);
+            return Ok("Book created !");
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult GetAll()
+        {
+            return Ok(this.bookService.GetAll());
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public ActionResult GetById(int id)
+        {
+            var res = this.bookService.GetBookById(id);
+
+            if (res != null) { return Ok(res); }
+
+            return NotFound("Book not found!");
+        }
+
+        // To Update Book
+        [HttpPut]
+        [Route("Update/{id}")]
+        public ActionResult UpdateBook(int id, Book updatedBook)
+        {
+            var result = this.bookService.UpdateBook(id, updatedBook);
+            if (result)
+                return Ok("Book updated successfully!");
+
+            return NotFound("Book not found!");
+        }
+
+        // To Delete Book
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public ActionResult DeleteBook(int id)
+        {
+            var result = this.bookService.DeleteBook(id);
+            if (result)
+                return Ok("Book deleted successfully!");
+
+            return NotFound("Book not found!");
+        }
+
+    }
+}
